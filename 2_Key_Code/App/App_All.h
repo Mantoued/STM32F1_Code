53e4cad3b3bec2ad5cf3d77d291c31ec3438c866
Mantoued <770136199@qq.com>
2023-03-25 21:43:15 +0800

    通过按键控制LED灯

---
+++ 2_Key_Code/App/App_All.h
@@ -0,0 +1,10 @@
+#ifndef __APP_ALL_H__
+#define __APP_ALL_H__
+
+#include "App_Gpio.h"
+#include "App_Tim.h"
+#include "App_Key.h"
+
+void App_All_Init(void);
+
+#endif
---
+++ 2_Key_Code/Bsp/Bsp_All.h
@@ -0,0 +1,10 @@
+#ifndef __BSP_ALL_H__
+#define __BSP_ALL_H__
+
+#include "Bsp_Gpio.h"
+#include "Bsp_Tim.h"
+#include "Bsp_Key.h"
+
+void Bsp_All_Init(void);
+
+#endif
